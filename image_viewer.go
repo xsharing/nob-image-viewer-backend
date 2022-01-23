f3524fa814d3fb34cfbc9c51a5172996c204d6ce
Nobuyuki Horiuchi <horiuchinobuyuki@gmail.com>
2022-01-23 17:41:01 +0900

    mock folders, images

--- image_viewer.go
+++ image_viewer.go
@@ -21,11 +21,18 @@ func NewImageViewer(logger *log.Logger) imageviewer.Service {
 // Folders implements folders.
 func (s *imageViewersrvc) Folders(ctx context.Context) (res []string, err error) {
 	s.logger.Print("imageViewer.folders")
-	return
+	return []string{"/", "/folder"}, nil
+}
+
+func Const2Ptr(str string) *string {
+	return &str
 }
 
 // Images implements images.
 func (s *imageViewersrvc) Images(ctx context.Context, p *imageviewer.ImagesPayload) (res []*imageviewer.Image, err error) {
 	s.logger.Print("imageViewer.images")
-	return
+	return []*imageviewer.Image{
+		{Name: Const2Ptr("a"), ThumbnailURL: Const2Ptr("https://dummyimage.com/200x200/000/00f"), OriginalURL: Const2Ptr("https://dummyimage.com/1280x1280/000/00f")},
+		{Name: Const2Ptr("b"), ThumbnailURL: Const2Ptr("https://dummyimage.com/200x200/000/00f"), OriginalURL: Const2Ptr("https://dummyimage.com/1280x1280/000/00f")},
+	}, nil
 }

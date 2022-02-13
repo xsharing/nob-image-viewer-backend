8e75aa15def862df1c3d892ef949bc5bc5e03826
Nobuyuki Horiuchi <horiuchinobuyuki@gmail.com>
2022-02-13 14:46:14 +0900

    get image

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.28.0
+	github.com/aws/aws-sdk-go v1.42.52
 	github.com/awslabs/aws-lambda-go-api-proxy v0.12.0
 	github.com/fnproject/fdk-go v0.0.15
 	github.com/tencentyun/scf-go-lib v0.0.0-20211123032342-f972dcd16ff6
--- s3_handler/s3_handler.go
+++ s3_handler/s3_handler.go
@@ -6,16 +6,39 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func handler(ctx context.Context, s3Event events.S3Event) {
+	session := session.Must(session.NewSession())
 	for _, record := range s3Event.Records {
 		s3 := record.S3
-		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+		// fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+		handle_file(session, s3.Bucket.Name, s3.Object.Key)
 	}
 }
 
 func main() {
 	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
+}
+
+func handle_file(session client.ConfigProvider, bucket string, key string) {
+	svc := s3.New(session)
+	params := &s3.GetObjectInput{
+		Bucket: aws.String(bucket), // Required
+		Key:    aws.String(key),    // Required
+	}
+	resp, err := svc.GetObject(params)
+
+	if err != nil {
+		fmt.Printf("error: %s %s %s", bucket, key, err.Error())
+		panic(err)
+	}
+
+	size := resp.ContentLength
+	fmt.Printf("retrieved: %s %s %d", bucket, key, size)
 }
